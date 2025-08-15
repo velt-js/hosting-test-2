36e1e072edeeb8f2bb8f67f14d78256a700b3d59
miri-san <mihir@velt.dev>
2025-08-15 14:02:00 +0530

    Update index.html title and favicon; comment out app-header in app.component.html; add dashboard route and adjust app.routes.ts; refine header component HTML and CSS for improved styling.

---
+++ src/app/pages/dashboard/dashboard.component.ts
@@ -0,0 +1,35 @@
+import { Component, inject, OnInit } from '@angular/core';
+import { AsyncPipe, CommonModule } from '@angular/common';
+import { ChatService } from 'src/app/services/chat.service';
+
+@Component({
+	selector: 'app-dashboard',
+	standalone: true,
+	imports: [AsyncPipe, CommonModule],
+	templateUrl: './dashboard.component.html',
+	styleUrl: './dashboard.component.css'
+})
+export class DashboardComponent implements OnInit {
+	chatService = inject(ChatService);
+	user$ = this.chatService.user$;
+	Math = Math;
+
+	recentActivity: { name: string, amount: number }[] = [];
+
+	ngOnInit(): void {
+		this.generateRandomActivity();
+	}
+
+	generateRandomActivity(): void {
+		const companies = ['ATCQ Inc.', 'De La Soul LLC', 'J Dilla Beats', 'MF DOOM Rhymes', 'Tribe Records'];
+		this.recentActivity = [];
+		for (let i = 0; i < 5; i++) {
+			const company = companies[Math.floor(Math.random() * companies.length)];
+			let amount = Math.floor(Math.random() * 1000) + 100;
+			if (Math.random() < 0.5) {
+				amount *= -1;
+			}
+			this.recentActivity.push({ name: company, amount: amount });
+		}
+	}
+}
